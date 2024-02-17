3854f12932d655e0f9c5779e525b0364b57d08bc
Morten Hjorth-Jensen <morten.hjorth-jensen@fys.uio.no>
2024-02-17 21:44:16 +0100

    Create example.py

---
+++ doc/src/week6/programs/example.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# Solution by brute force inversion
+A = np.array([[2,1],[1,20]])
+b = np.array([5,3])
+invA = np.linalg.inv(A)
+x = invA @ b
+print(x)
+print(np.linalg.eig(A))
+#  Solution with iterative methods
+
+# Solution with gradient descent and fixed learning rate
