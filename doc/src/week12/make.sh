6e275885cd2eff2269560c7fcf74c7e6c3a86392
Morten Hjorth-Jensen <morten.hjorth-jensen@fys.uio.no>
2026-04-16 21:52:53 +0200

    update

--- doc/src/week12/make.sh
+++ doc/src/week12/make.sh
@@ -34,26 +34,11 @@ system doconce format ipynb $name $opt  --no_abort
 
 
 
-# Ordinary plain LaTeX document
-rm -f *.aux  # important after beamer
-system doconce format pdflatex $name --minted_latex_style=trac --latex_admon=paragraph $opt
-system doconce ptex2tex $name envir=minted
-# Add special packages
-doconce subst "% Add user's preamble" "\g<1>\n\\usepackage{simplewick}" $name.tex
-doconce replace 'section{' 'section*{' $name.tex
-pdflatex -shell-escape $name
-pdflatex -shell-escape $name
-mv -f $name.pdf ${name}.pdf
-cp $name.tex ${name}-plain.tex
-
-
 
 # Publish
 dest=../../pub
 if [ ! -d $dest/$name ]; then
 mkdir $dest/$name
-mkdir $dest/$name/pdf
-mkdir $dest/$name/html
 mkdir $dest/$name/ipynb
 fi
 cp ${name}*.pdf $dest/$name/pdf
